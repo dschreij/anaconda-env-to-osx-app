b2c6d0670567d2225dc1e1288ab7aac14b5ed91a
Daniel Schreij <dschreij@gmail.com>
2016-07-10 02:03:10 +0200

    Moved al OS specific stuff to settings.py
    - better variable checking and handling. Uses defaults for some
    variables if they are not specified.
    - Updated README

--- README.md
+++ README.md
@@ -1,12 +1,20 @@
-This script creates Mac OS X apps and dmgs from Anaconda Python environments. 
-The benefit of doing this instead of using py2app is:
+This script creates Mac OS X apps and dmgs from Anaconda Python environments. Anaconda environments are already isolated from the rest of the system, as in, all dependencies of conda packages are contained in the environment itself and no packages outside of the environment should be required. This makes an Anaconda environment distributable 'as is'. It just needs to be packaged as an app. That's what this script does.
+
+The benefit of doing it like this instead of using py2app is:
 
 - The process is much more transparent, basically your app will be a direct copy of the
-Anaconda environment with no things changed, and all othe steps can be followed in the script.
-- The resulting app will not be frozen. This makes it easier to maintain or (auto)update it 
-- Best of all, multiprocessing (in combination with billiard and spawn) will work in Python 2.
+Anaconda environment with no things changed, and all othe steps can be followed or changed in the script.
+- The resulting app will not be frozen. This makes it easier to maintain or (auto) update it.
+- Best of all, multiprocessing (in combination with billiard and spawn) will work in Python 2. I didn't get this to happen with billiard+py2app.
 
 ## Dependencies
 - biplist
 - six
 - dmgbuild
+
+## Usage
+Create a python script as a configuration file, and pass it as a command line argument to conda_env_to_app, e.g.:
+
+    ./conda_env_to_app settings.py
+
+This settings file should specify all the necessary variables that are required to generate the app (and the dmg if desired). See the included settings.py as an example. This settings file was created to package the [OpenSesame experiment builder](http://github.com/smathot/OpenSesame).
--- settings.py
+++ settings.py
@@ -1,39 +1,196 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import osxrelocator
+import os
+import re
 
-from AppKit import NSScreen
-frame = NSScreen.mainScreen().frame()
-xres = frame.size.width
-yres = frame.size.height
+#===============================================================================
+# General settings applicable to all apps
+#===============================================================================
 
+# Name of the app
 APP_NAME = "OpenSesame"
+# The short version string
 VERSION = "3.1.0"
+# The website in reversered order (domain first, etc.)
 IDENTIFIER = "nl.cogsci.osdoc"
-APP_EXT = "osexp"
+# The author of this package
 AUTHOR = "Sebastiaan Mathot"
-CONDA_ENV_PATH = "/Users/daniel/anaconda/envs/OpenSesame"
-CONDA_FOLDERS = ["lib", "bin", "share", "conda-meta", "qsci", "ssl", "translations"]
-ICON_PATH = "/Users/daniel/Github/OpenSesame/opensesame_resources/opensesame.icns"
+# Path to the anaconda environment folder to package
+CONDA_ENV_PATH = "~/anaconda/envs/OpenSesame"
+# Folders to include from Anaconda environment, if ommitted everything will be
+# copied
+CONDA_FOLDERS = ["lib", "bin", "share", "qsci", "ssl", "translations"]
+# Paths of files and folders to remove from the copied anaconda environment, 
+# relative to the environment's root. 
+# For instance, this could be the qt4 apps (an app inside an app is useless)
+CONDA_EXCLUDE_FILES = [
+	'bin/*-qt4*'
+]
+# Path to the icon of the app
+ICON_PATH = "~/Github/OpenSesame/opensesame_resources/opensesame.icns"
+# The entry script of the application in the environment's bin folder
 ENTRY_SCRIPT = "opensesame"
-LOCAL_LIB_FOLDER = "/usr/local/lib"
+# Folder to place created APP and DMG in.
 OUTPUT_FOLDER = "~/EXPERIMENTAL/"
+# Obligatory Info.plist setting, see:
+# https://developer.apple.com/library/ios/documentation/General/Reference/InfoPlistKeyReference/Articles/CoreFoundationKeys.html
+# Should be one of {Editor, Viewer, Shell, None}
 CFBUNDLETYPEROLE = "Editor"
-LSItemContentTypes = "public.item"
+
+# Information about file types that the app can handle
+APP_SUPPORTED_FILES = [{
+	'CFBundleTypeExtensions': ["osexp"],
+	'CFBundleTypeIconFile': os.path.basename(ICON_PATH),
+	'LSItemContentTypes': ["public.item"] 
+}]
 
 # ===== Settings specific to dmgbuild =====
 
-DMG_FILE = 'opensesame_{}-py2.7-macos-1.dmg'.format(VERSION)
+# Create a DMG template name, so version can be overwritten if it can be 
+# determined from the OS libraries.
+os_dmg_template = 'opensesame_{}-py2.7-macos-1.dmg'
+
+# Name of the DMG file that will be created in OUTPUT_FOLDER
+DMG_FILE = os_dmg_template.format(VERSION)
+# DMG format
 DMG_FORMAT = 'UDBZ'
-DMG_BACKGROUND = "/Users/daniel/Github/OpenSesame/opensesame_resources/einstein.png"
+# Locations of shortcuts in DMG window
 DMG_ICON_LOCATIONS = {
 	APP_NAME + '.app': (30, 450),
 	'Applications' : (300, 450)
 }
-
-# Center the window
-win_size = (358, 570)
-x_win = xres/2 - win_size[0]/2
-y_win = yres/2 - win_size[1]/2
-
-DMG_WINDOW = ((x_win, y_win), win_size)
+# Size of DMG window when mounted
+DMG_WINDOW_RECT = ((300, 200), (358, 570))
+# Size of icons in DMG
 DMG_ICON_SIZE = 80
+
+# Background of DMG file
+DMG_BACKGROUND = "~/Github/OpenSesame/opensesame_resources/einstein.png"
+
+#===============================================================================
+# Extra settings and functions specific to OpenSesame (Remove for other apps)
+#===============================================================================
+
+LOCAL_LIB_FOLDER = "/usr/local/lib"
+
+# Try to obtain OpenSesame version from OpenSesame source
+os_metadata_file = os.path.expanduser(os.path.join(CONDA_ENV_PATH, 'lib', 'python2.7',
+	'site-packages', 'libopensesame', 'metadata.py'))
+try:
+	with open(os_metadata_file, 'r') as fp:
+		metadata = fp.read()
+except Exception as e:
+	print("Could not read OpenSesame version from metadata: {}".format(e))
+else:
+	version_match = re.search("(?<=__version__)\s*=\s*u'(.*)'", metadata)
+	if version_match:
+		VERSION = version_match.group(1)
+		
+	codename_match = re.search("(?<=codename)\s*=\s*u'(.*)'", metadata)
+	if codename_match:
+		codename = codename_match.group(1)
+		LONG_VERSION = VERSION + ' ' + codename
+	else:
+		LONG_VERSION = VERSION
+
+	# Overwrite name of the DMG file that will be created in OUTPUT_FOLDER
+	DMG_FILE = os_dmg_template.format(VERSION)
+
+	print("Creating app for {} {}".format(APP_NAME, LONG_VERSION))
+
+def extra():
+	copy_opensesame_with_py_ext()
+	# copy_libpng()
+	copy_sdl_libraries()
+	correct_pygame_links()
+
+def copy_opensesame_with_py_ext():
+	""" Copy bin/opensesame to bin/opensesame.py to enable multiprocessing """
+	shutil.copy(
+		os.path.join(RESOURCE_DIR, 'bin', ENTRY_SCRIPT),
+		os.path.join(RESOURCE_DIR, 'bin', ENTRY_SCRIPT + '.py')
+	)
+
+def copy_libpng():
+	""" Copy newer (Homebrew) libpng to Resources folder, 
+	as the Anaconda one is too old """
+	print("Copying libpng16.16.dylib")
+	try:
+		shutil.copyfile('/usr/local/opt/libpng/lib/libpng16.16.dylib',
+			os.path.join(RESOURCE_DIR, 'lib', 'libpng16.16.dylib'))
+	except OSError as e:
+		print("Error copying libpng: {}".format(e))
+
+def copy_sdl_libraries():
+	""" Copy SDL files into Resources/lib """
+	print("Copying SDL library from {} to {}".format(LOCAL_LIB_FOLDER,
+		os.path.join(RESOURCE_DIR, 'lib')))
+
+	SDL_files = [
+		'libSDL-1.2.0.dylib',
+		'libSDL_image-1.2.0.dylib',
+		'libSDL_mixer-1.2.0.dylib',
+		'libSDL_ttf-2.0.0.dylib',
+		'libportmidi.dylib',
+		'libportaudio.2.dylib',
+	]
+
+	try:
+		for SDL_lib in SDL_files:
+			shutil.copy2(
+				os.path.join(LOCAL_LIB_FOLDER, SDL_lib),
+				os.path.join(RESOURCE_DIR, 'lib', SDL_lib),
+			)
+			os.chmod(os.path.join(RESOURCE_DIR, 'lib', SDL_lib), 0o755)
+	except OSError as e:
+		print("Could not copy SDL libraries: e".format(e))
+
+	print("========== Correcting internal references of SDL files ==========")
+
+	relocator = osxrelocator.OSXRelocator(
+		RESOURCE_DIR + '/lib', '/usr/local/lib',
+		'@rpath/', False)
+	relocator.relocate()
+
+	relocator = osxrelocator.OSXRelocator(
+		RESOURCE_DIR + '/lib',
+		'/usr/local/opt/sdl_image/lib/',
+		'@rpath/', False)
+	relocator.relocate()
+
+	relocator = osxrelocator.OSXRelocator(
+		RESOURCE_DIR + '/lib',
+		'/usr/local/opt/sdl/lib/',
+		'@rpath/', False)
+	relocator.relocate()
+
+def correct_pygame_links():
+	""" Correct the internal links of pygame to SDL """
+	print("========== Correcting pygame links to SDL ==========")
+	pygame_path = os.path.join(RESOURCE_DIR, 'lib',
+		'python{}.{}'.format(sys.version_info.major, sys.version_info.minor),
+		'site-packages','pygame')
+
+	relocator = osxrelocator.OSXRelocator(
+		pygame_path,
+		LOCAL_LIB_FOLDER,
+		'@rpath/',
+		False)
+	relocator.relocate()
+
+	relocator = osxrelocator.OSXRelocator(
+		pygame_path,
+		'/usr/local/opt/sdl_image/lib/',
+		'@rpath/',
+		False)
+	relocator.relocate()
+
+	relocator = osxrelocator.OSXRelocator(
+		pygame_path,
+		'/usr/local/opt/libpng/lib/',
+		'@rpath/',
+		False)
+	relocator.relocate()
+
+
